e94ce2aaa1f5a9e0dbe02c9e1145d4c68e9ef717
Artanoah <artanoah@web.de>
2014-11-24 20:16:19 +0100

    Dinge

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -6,22 +6,67 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import models.Angebot;
+import models.Fertigungsauftrag;
+import models.Kundenauftrag;
+import models.Transportauftrag;
+import GUI.ClientGUI;
+import bossKomponente.IMPS;
 import utils.*;
 import distributedExtension.*;
 
-public class Client {
+public class Client implements IMPS {
 	
 	private IDispatcherToClient dispatcher = null;
+	private ClientGUI gui;
 	
 	//Socket aufbauen
 	public Client(IDispatcherToClient dispatcher) throws UnknownHostException, IOException{
 		this.dispatcher = dispatcher;
 	}
 
-	
-	
-	
+	@Override
+	public Date berechneFertigungszeitpunkt(int fertigungsAuftragId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Fertigungsauftrag erstelleFertigungsauftrag(Angebot angebot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Transportauftrag erstelleTransportauftrag(Angebot angebot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Kundenauftrag erstelleKundenauftrag(Angebot angebot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double berechneKosten(Angebot angebot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Angebot erstelleAngebot(Map<Integer, Integer> matNrZuMenge,
+			int kundenNr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void start() {
+		gui = new ClientGUI();
+	}
 }

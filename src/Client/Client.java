438684460e9e188e7fe07337b087c925d55a04f3
BirgerK <Birger.kamp@googlemail.com>
2014-11-24 19:59:46 +0100

    mach mal client nu

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,7 @@
 package Client;
 
+import interfaces.IDispatcherToClient;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -11,35 +13,13 @@ import distributedExtension.*;
 
 public class Client {
 	
-	SocketConnection sConnection = null;
-	String hostName = null;
-	InetAddress ipAdress = null;
+	private IDispatcherToClient dispatcher = null;
 	
 	//Socket aufbauen
-	private void Client(String hostName) throws UnknownHostException, IOException{
-		this.hostName = hostName;
+	private void Client(IDispatcherToClient dispatcher) throws UnknownHostException, IOException{
+		this.dispatcher = dispatcher;
 	}
 
-	private void Client(InetAddress ipAddress) throws UnknownHostException, IOException{
-		this.ipAdress = ipAddress;
-	}
-	// Im Dispatcher Server hinzufügen
-	
-	
-	//Funktionen mit Argumenenten aufrufen
-	public ResultMessage anfrageSenden(String command, List<Object> arguments) throws IOException, ClassNotFoundException {
-		if (ipAdress == null && hostName == null)throw new SocketException("Socket isn't initalized");
-		if(hostName == null && ipAdress != null)sConnection = new SocketConnection(this.ipAdress, Constants.MPS_DISPATCHER_PORT);
-		if(ipAdress == null && hostName != null)sConnection = new SocketConnection(this.hostName, Constants.MPS_DISPATCHER_PORT);
-		
-		MethodInvokeMessage message = new MethodInvokeMessage(command, arguments);
-
-		sConnection.writeObject(message);
-		ResultMessage result = (ResultMessage) sConnection.readObject();
-		
-		return result;
-
-	}
 	
 	
 	

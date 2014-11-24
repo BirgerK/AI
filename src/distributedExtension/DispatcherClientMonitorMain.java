a9040a6f0536a6b431be221dc8c77d6ef4b4a027
Artanoah <artanoah@web.de>
2014-11-24 20:11:07 +0100

    Dinge

--- src/distributedExtension/DispatcherClientMonitorMain.java
+++ src/distributedExtension/DispatcherClientMonitorMain.java
@@ -1,20 +1,22 @@
 package distributedExtension;
 
+import Client.Client;
 import GUI.Monitor;
 
 public abstract class DispatcherClientMonitorMain {
 	private static Dispatcher dispatcher = null;
 	private static Monitor monitor = null;
+	private static Client client = null;
 	
 	public static void main(String[] args) {
 		dispatcher = new Dispatcher();
-		//client = new client(dispatcher);
+		client = new Client(dispatcher);
 		monitor = new Monitor(dispatcher);
 		
 		
 		StartServerService.main(null);
 		dispatcher.start();
-		//client.start();
+		client.start();
 		monitor.start();
 		//hoffen dass es laeuft
 	}
